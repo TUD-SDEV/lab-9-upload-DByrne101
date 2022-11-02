a03f80b7ed2bec1c65dbdf67242f149d2d082519
X00185345 <X00185345@mytudublin.ie>
2022-11-02 20:31:23 +0000

    lab 9 part 1 commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, ProfileEditView, ProfilePageView
 
 urlpatterns=[
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('edit_profile/<int:pk>/', ProfilePageView(), name='show_profile')
 ]
